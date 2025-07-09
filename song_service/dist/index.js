ec0dbf57530b4516634e469ef3693a96062c7387
SHANKAR-YADAVA <shankaryadavar2004@gmail.com>
2025-07-09 13:05:01 +0530

    Implement Redis caching for album and song retrieval in controllers

--- song_service/dist/index.js
+++ song_service/dist/index.js
@@ -1,11 +1,23 @@
 import express from "express";
 import doetnv from "dotenv";
+import songRoutes from "./route.js";
+import redis from "redis";
 import cors from "cors";
-import songroutes from "./route.js";
 doetnv.config();
+export const redisClient = redis.createClient({
+    password: process.env.Redis_Password,
+    socket: {
+        host: "redis-14223.c301.ap-south-1-1.ec2.redns.redis-cloud.com",
+        port: 14223,
+    },
+});
+redisClient
+    .connect()
+    .then(() => console.log("connected to redis"))
+    .catch(console.error);
 const app = express();
 app.use(cors());
-app.use("/api/v1", songroutes);
+app.use("/api/v1", songRoutes);
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`server is running on ${port}`);
